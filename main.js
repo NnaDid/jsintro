83da17cb8d65fe01d5bd95eec35ecbb10f6cd25e
MaxEfuru <fredrickmaximum@gmail.com>
2023-01-10 13:16:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+console.log("I am a simple application");
+// alert("I am a simple application");
+
+// var y ;   // variable declration
+// y = 20;   // variable initialization
+
+// var x = 1000;
+
+// let z = 250;
+
+// const w = 78;   // constant variable
+
+
+// z =290;
+
+// console.log(z);
+
+
+
+let welcome = document.getElementById("welcome");
+welcome.innerHTML = "Welcome To Javascript Code";
+
+console.log(window);
+
+const nameElmen  = document.getElementById("name");
+const nameInput  = document.querySelector("nameInput");
+const okay       = document.querySelector("ok");
+
+
+
+
+// let username = prompt("What is is your name?","");
+
+//    nameElmen.innerHTML = username;
+
+
+
+
+   
+
+
+
+
